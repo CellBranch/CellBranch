76ea28dc1113b77cfeecd95675986d3ce8e7e9e9
Dr R Greaves <rg531@pc405h.cs.york.ac.uk>
2015-10-26 10:19:14 +0000

    updated do_batch_of_200.sh and README to alert users of need to specify the path to java on their local cluster, rather than using the York-specific values currently in the scripts

--- cluster_scripts/do_batch_of_200.sh
+++ cluster_scripts/do_batch_of_200.sh
@@ -4,5 +4,8 @@
 #$ -t 1-200
 #$ -l h_rt=00:25:00
 #$ -l h_vmem=4G
-/opt/yarcc/infrastructure/java/1.7.0_60/1/default/bin/java -server -Xms300m -Xmx300m -classpath CBSimv1.0b.jar:mason.16.jar simv0.CBSimulation n8_o0_parameters.xml "${1}/${2}/${SGE_TASK_ID}"
+# replace the variable $path_to_local_java with the correct file path of the version of java located on your cluster
+# for the University of York , YARCC, this is 
+# /opt/yarcc/infrastructure/java/1.7.0_60/1/default/bin/java
+$path_to_local_java -server -Xms300m -Xmx300m -classpath CBSimv1.0b.jar:mason.16.jar simv0.CBSimulation n8_o0_parameters.xml "${1}/${2}/${SGE_TASK_ID}"
 date
